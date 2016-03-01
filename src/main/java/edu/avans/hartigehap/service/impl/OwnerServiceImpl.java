d32bb78ff94c5537c7b0126ca63012bcd4e3ac52
Xander Gerreman <xander@gerreman.nl>
2016-03-01 09:53:22 +0100

    bug fix log4j

--- src/main/java/edu/avans/hartigehap/service/impl/OwnerServiceImpl.java
+++ src/main/java/edu/avans/hartigehap/service/impl/OwnerServiceImpl.java
@@ -3,7 +3,6 @@ package edu.avans.hartigehap.service.impl;
 import java.util.Arrays;
 import java.util.List;
 
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
