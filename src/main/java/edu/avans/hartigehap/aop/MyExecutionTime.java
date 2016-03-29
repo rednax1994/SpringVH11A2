e884eff9b0b6e3d40c3218c8324dea1de9b53746
Xander Gerreman <xander@gerreman.nl>
2016-03-29 10:54:07 +0200

    exclude from sonarqube

--- src/main/java/edu/avans/hartigehap/aop/MyExecutionTime.java
+++ src/main/java/edu/avans/hartigehap/aop/MyExecutionTime.java
@@ -5,6 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@SuppressWarnings("squid:S00112")
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface MyExecutionTime {
