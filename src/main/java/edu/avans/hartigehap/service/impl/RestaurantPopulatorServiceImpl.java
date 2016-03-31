2f8d2915e3285aa961eb663c3fb53ae9bd6ff60f
Xander Gerreman <xander@gerreman.nl>
2016-03-31 10:39:28 +0200

    fix in debugging

--- src/main/java/edu/avans/hartigehap/service/impl/RestaurantPopulatorServiceImpl.java
+++ src/main/java/edu/avans/hartigehap/service/impl/RestaurantPopulatorServiceImpl.java
@@ -325,9 +325,9 @@ public class RestaurantPopulatorServiceImpl implements RestaurantPopulatorServic
             res = reservationRepository.save(res);
             res.getCurrentState().acceptReservation();
         } catch (MyException e) {
-            log.debug("" + e.getMessage());
+            new MyException("error in RestaurantPopulatorImpl: ", e);
         } catch (StateException e) {
-            log.debug("" + e.getMessage());
+            new StateException("StateException in RestaurantPopulatorImpl: " + e);
         }
     }
 }
