ef8b8facd90331b37ec9b97e0dc1d9365390b8fc
Xander Gerreman <xander@gerreman.nl>
2016-03-31 11:13:21 +0200

    direct return

--- src/main/java/edu/avans/hartigehap/service/impl/ReservationServiceImpl.java
+++ src/main/java/edu/avans/hartigehap/service/impl/ReservationServiceImpl.java
@@ -2,8 +2,6 @@ package edu.avans.hartigehap.service.impl;
 
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
@@ -16,33 +14,32 @@ import edu.avans.hartigehap.domain.reservationfactory.TableReservation;
 import edu.avans.hartigehap.repository.RoomReservationRepository;
 import edu.avans.hartigehap.repository.TableReservationRepository;
 import edu.avans.hartigehap.service.ReservationService;
-import lombok.extern.slf4j.Slf4j;
 
 @Service("ReservationService")
 @Repository
 @Transactional
-@Slf4j
 public class ReservationServiceImpl implements ReservationService {
-    private static final Logger LOGGER = LoggerFactory.getLogger(ReservationServiceImpl.class);
-
+    
     @Autowired
     private RoomReservationRepository roomReservationRepository;
-
+    
     @Autowired
     private TableReservationRepository tableReservationRepository;
-
+    
     @Override
     @Transactional(readOnly = true)
     public List<RoomReservation> findReservationsForRoom(Room room) {
-
-        List<RoomReservation> reservationsForRoom = roomReservationRepository.findByRoom(room);
-        return reservationsForRoom;
+        @SuppressWarnings("unused")
+        List<RoomReservation> reservationsForRoom;
+        return reservationsForRoom = roomReservationRepository.findByRoom(room);
+        
     }
-
+    
     @Override
     @Transactional(readOnly = true)
-    public List<TableReservation> findReservationsForDiningTable(DiningTable diningTable){
-        List<TableReservation> reservationsForDiningTable = tableReservationRepository.findByDiningTable(diningTable);
-        return reservationsForDiningTable;
+    public List<TableReservation> findReservationsForDiningTable(DiningTable diningTable) {
+        @SuppressWarnings("unused")
+        List<TableReservation> reservationsForDiningTable;
+        return reservationsForDiningTable = tableReservationRepository.findByDiningTable(diningTable);
     }
 }
